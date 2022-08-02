546ac36f9abb5fb7d2b54a6f7245a86229a0723f
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2022-08-02 22:21:08 +0300

    final

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,35 +4,64 @@ import { InputControlled, InputUncontrolled } from '../Input02/Input02'
 class Form extends Component {
     state = {
         inputRef: React.createRef(),
-        personName: 'chris'
+        personName: '',
+        users: []
     };
     onSubmit = (event) => {
         event.preventDefault();
         console.log({ event }, 'submitted')
+        this.setState({
+            users: [...this.state.users, this.state.personName],
+            personName: ''
+        })
+        if (this.state.inputRef && this.state.inputRef.current) {
+            console.log(this.state.inputRef.current.value);
+            this.setState({
+                users: [...this.state.users, this.state.inputRef.current.value],
+                // inputRef: this.state.inputRef.current.value,
+            })
+        }
+
     }
-    onChange = (event) =>
+    onChange = (event) => {
         this.setState({ [event.target.name]: event.target.value });
-    // console.log(event.target.name, event.target.value);
+        // console.log(event.target.name, event.target.value);
+        // this.setState({ user: this.state.users.push() })
+    }
+    deleteUser = (user) => {
+        this.setState({ users: this.state.users.filter((personName) => personName !== user) })
+    }
 
     render() {
         // console.log(this.state.inputRef);
         console.log(this.state);
         return (
-            <form onSubmit={this.onSubmit}>
-                <InputControlled
-                    name='personName'
-                    type='text'
-                    value={this.state.personName}
-                    onChange={this.onChange}
-                />
-                <br />
-                <InputUncontrolled
-                    defaultValue={'Chris'}
-                    inputRef={this.state.inputRef}
-                />
-                <br />
-                <button type='submit'>submit</button>
-            </form>
+            <div>
+                <form onSubmit={this.onSubmit}>
+                    <InputControlled
+                        name='personName'
+                        type='text'
+                        value={this.state.personName}
+                        onChange={this.onChange}
+                    />
+                    <br />
+                    <InputUncontrolled
+                        defaultValue={'Chris'}
+                        inputRef={this.state.inputRef}
+                    />
+                    <br />
+                    <button type='submit'>submit</button>
+                </form>
+                {this.state.personName.length < 2 && <span>Error</span>}
+                <ul>
+                    {this.state?.users?.map((user) => (
+                        <>
+                            <li key={user}>{user}</li>
+                            <button onClick={() => this.deleteUser(user)}>X</button>
+                        </>
+                    ))}
+                </ul>
+            </div>
         )
     }
 }
